5dccc060a93503c1f2e1cd770e2a167a08fa1291
vertulloa <20638889+AmedeoV@users.noreply.github.com>
2022-05-25 14:34:06 +0100

    Increase memory, gif of load test

--- MemoryLeakApi/MemoryLeakApi/soakTest.js
+++ MemoryLeakApi/MemoryLeakApi/soakTest.js
@@ -4,14 +4,12 @@ import { check, sleep } from 'k6';
 
 export const options = {
     stages: [
-        // { duration: '20s', target: 10000 },
-        { duration: '3m', target: 200 },
-        { duration: '2m', target: 100 },
-        { duration: '1m', target: 10 },
+        { duration: '30s', target: 10 },
+        { duration: '5s', target: 10 },
         { duration: '1s', target: 1 },
     ],
 };
-export default function (data) {
+export default function () {
 
     const res = http.get('http://localhost/api/MemoryLeak/memleak/20000');
 
